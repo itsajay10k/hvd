8951d2f084a794a24e6c4c008c531a2bc2187027
itsajay10k <158288992+itsajay10k@users.noreply.github.com>
2024-02-03 19:31:29 +0530

    happy valentines day

---
+++ hvd.js
@@ -0,0 +1,12 @@
+let name2 = confirm("Can You Print Your Name?");
+// alert("Please Enter Your Name");
+function call() {
+    let name1 = prompt("Enter Your Name!");
+    document.getElementById("max").innerText = "I Love you " + name1 + " \u2665";
+}
+
+
+if (name2){
+    call();
+};
+    // document.getElementById("max").innerText = "I Love you my jaan \u2665";
